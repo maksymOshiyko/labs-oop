b7d55fc0f5c224f224a0d5ca04123e26c47f6cf7
Max Oshyiko <maksym.oshiyko@gmail.com>
2020-09-30 23:46:33 +0300

    dz3

---
+++ dz3/Converter/Converter/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+
+namespace Converter
+{
+    class Program
+    {
+        private class Converter
+        {
+            private double _usd;
+            private double _eur;
+            public Converter(double usd, double eur)
+            {
+                this._usd = usd;
+                this._eur = eur;
+            }
+
+            public double UAHToUSD(double uah)
+            {
+                return uah / _usd;
+            }
+            public double UAHToEUR(double uah)
+            {
+                return uah / _eur;
+            }
+            public double USDToUAH(double usd)
+            {
+                return usd * _usd;
+            }
+            public double USDToEUR(double usd)
+            {
+                return usd * (_usd / _eur);
+            }
+            public double EURToUSD(double eur)
+            {
+                return eur * (_eur / _usd);
+            }
+            public double EURToUAH(double eur)
+            {
+                return eur * _eur;
+            }
+
+            public double Convert(string from, string to, double value) // -1 = не вірні вхідні дані
+            {
+                string[] currencies = { "usd", "uah", "eur" };
+                bool checkFrom = false; // перевірка на вірність вводу from
+                bool checkTo = false; // перевірка на вірність вводу to
+
+                foreach(string currency in currencies)
+                {
+                    if(from == currency)
+                    {
+                        checkFrom = true;
+                    }
+
+                    if(to == currency)
+                    {
+                        checkTo = true;
+                    }
+                }
+
+                if(checkTo == false || checkFrom == false)
+                {
+                    return -1;
+                }
+
+
+                if(from == to)
+                {
+                    return value;
+                }
+
+                if (from == "uah")
+                {
+                    if (to == "usd")
+                    {
+                        return this.UAHToUSD(value);
+                    }
+                    else
+                    {
+                        return this.UAHToEUR(value);
+                    }
+                }
+                else if (from == "eur")
+                {
+                    if (to == "usd")
+                    {
+                        return this.EURToUSD(value);
+                    }
+                    else
+                    {
+                        return this.EURToUAH(value);
+                    }
+                }
+                else if (from == "usd")
+                {
+                    if (to == "eur")
+                    {
+                        return this.USDToEUR(value);
+                    }
+                    else
+                    {
+                        return this.USDToUAH(value);
+                    }
+                }
+                else
+                {
+                    return -1;
+                }
+
+                
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Максим Ошийко К-24");
+
+            Console.WriteLine("Введiть курс долара по вiдношенню до гривнi: ");
+            double usd = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введiть курс євро по вiдношенню до гривнi: ");
+            double eur = Double.Parse(Console.ReadLine());
+
+            Converter conv = new Converter(usd, eur);
+
+            bool exit = false;
+
+            while (!exit)
+            {
+                Console.WriteLine("З якоi валюти перевести?(uah, eur, usd) ");
+                string from = Console.ReadLine();
+
+                Console.WriteLine("В яку валюту переводити?(uah, eur, usd) ");
+                string to = Console.ReadLine();
+
+                Console.WriteLine("Введiть сумму: ");
+                double value = Double.Parse(Console.ReadLine());
+
+                double result = conv.Convert(from, to, value);
+
+                if (result == -1) // не вірні дані
+                {
+                    Console.WriteLine("Не вiрнi вхiднi данi. Спробуйте ще раз.");
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine($"{value} {from} = {result} {to}");
+                }
+
+                Console.WriteLine("Продовжити конвертацiю?(Y,N) ");
+                string s = Console.ReadLine();
+
+                if(s == "N")
+                {
+                    Console.WriteLine("Роботу програми завершено.");
+                    exit = true;
+                }
+            }
+        }
+    }
+}
---
+++ dz3/GroupApp/GroupApp/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Encodings;
+
+namespace GroupApp
+{
+    class Program
+    {
+        private abstract class Student
+        {
+            public string Name { get; set; }
+            public string State { get; set; } 
+
+            public Student(string name)
+            {
+                this.Name = name;
+                this.State = "";
+            }
+
+            public void Read()
+            {
+                this.State += "Read ";
+            }
+
+            public void Write()
+            {
+                this.State += "Write ";
+            }
+
+            public void Relax()
+            {
+                this.State += "Relax "; 
+            }
+
+            public abstract void Study();
+        } 
+
+        private class GoodStudent : Student
+        {
+            public GoodStudent(string name) : base(name)
+            {
+                this.State = "good ";
+            }
+
+            public override void Study()
+            {
+                this.Read();
+                this.Write();
+                this.Read();
+                this.Write();
+                this.Relax();
+            }
+        }
+
+        private class BadStudent : Student
+        {
+            public BadStudent(string name) : base(name)
+            {
+                this.State = "bad ";
+            }
+
+            public override void Study()
+            {
+               this.Relax();
+               this.Relax();
+               this.Relax();
+               this.Relax();
+               this.Read();
+            }
+        }
+
+        private class Group
+        {
+            public string Name { get; set; }
+            public List<Student> Students { get; set; }
+
+            public Group(string name)
+            {
+                this.Name = name;
+                this.Students = new List<Student>();
+            }
+
+            public void AddStudent(Student student)
+            {
+                this.Students.Add(student);
+            }
+
+            public void Study()
+            {
+                foreach(Student student in this.Students)
+                {
+                    student.Study();
+                }
+            }
+
+            public void GetInfo()
+            {
+                Console.Write($"{this.Name} ");
+                foreach(Student student in this.Students)
+                {
+                    Console.Write($"{student.Name} ");
+                }
+                Console.Write("\n");
+            }
+
+            public void GetFullInfo()
+            {
+                Console.WriteLine($"{this.Name} ");
+                foreach (Student student in this.Students)
+                {
+                    Console.WriteLine($"{student.Name} {student.State}");
+                }
+                Console.Write("\n");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            Console.WriteLine("Максим Ошийко К-24");
+
+            while (!exit)
+            {
+                Console.Write("Введiть назву групи: ");
+                string groupName = Console.ReadLine();
+
+                Console.Write("Введiть iмена 3 студентiв через пробiл: ");
+                string[] studentNames = Console.ReadLine().Split(' ');
+                
+                Group group = new Group(groupName);
+                group.AddStudent(new GoodStudent(studentNames[0]));
+                group.AddStudent(new BadStudent(studentNames[1]));
+                group.AddStudent(new GoodStudent(studentNames[2]));
+
+                group.Study();
+
+                Console.WriteLine("Бажаєте переглянути часткову(1) чи повну(2) iнформацiю? ");
+                int num = Int32.Parse(Console.ReadLine());
+
+                if(num == 1)
+                {
+                    group.GetInfo();
+                }
+                else
+                {
+                    group.GetFullInfo();
+                }
+
+                Console.WriteLine("Бажаєте створити нову групу(1) чи завершити виконання програми(2)?");
+                num = Int32.Parse(Console.ReadLine());
+
+                if(num == 2)
+                {
+                    exit = true;
+                }
+                else
+                {
+                    exit = false;
+                }
+            }
+
+            Console.WriteLine("Программа завершена.");
+        }
+    }
+}
